7c823336e1b46055279da301979366c6b90a1f58
Piekna Alicja <alicjamysliwiec16@gmail.com>
2022-01-02 19:23:49 +0100

    Add alt to images

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ export default function Photos(props) {
                         return (
                             <div className="col-lg-4" key={index}>
                                 <a href={photo.src.original} rel="noopener noreferrer" target=" _blank" >
-                                    < img className="img-fluid p-2" src={photo.src.landscape} alt="photo-gallery" />
+                                    < img className="img-fluid p-2" src={photo.src.landscape} alt="gallery" />
                                 </a>
                             </div>
                         )
