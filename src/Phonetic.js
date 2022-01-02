49ef05e3f5812f397e034e974ae2b802e22e42a7
Piekna Alicja <alicjamysliwiec16@gmail.com>
2022-01-02 17:33:17 +0100

    Fixes v2

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
     return (
         <div className="Phonetic">
             {props.phonetic.audio &&
-                <a className="listen-button" rel="noopener noreferrer" href={props.phonetic.audio} rel="noreferrer" target="_blank" >
+                <a className="listen-button" rel="noopener noreferrer" href={props.phonetic.audio} target="_blank" >
                     <span aria-label="listen-emoji" role="img">
                         🔊
                     </span>
