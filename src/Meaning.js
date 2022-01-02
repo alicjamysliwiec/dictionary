44f2c67d62b87004e42e44f066ad374f71a9bacc
Piekna Alicja <alicjamysliwiec16@gmail.com>
2022-01-02 16:50:51 +0100

    add synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,20 +2,43 @@ import "./Meaning.css";
 import React from "react";
 
 export default function Meaning(props) {
-  return (
-    <div className="meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function(definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="meaning">
+            <h3>{props.meaning.partOfSpeech}</h3>
+            {props.meaning.definitions.map(function (definition, index) {
+                return (
+                    <div key={index}>
+                        <hr />
+                        <p>
+                            <strong>
+                                Definition:
+                            </strong> {definition.definition}
+                            {definition.example &&
+                                <>
+                                    <br />
+                                    <strong>
+                                        Example:
+                                    </strong>
+                                    <em>{definition.example}</em>
+                                </>}
+                            {definition.synonyms.length !== 0 &&
+                                <>
+                                    <br />
+                                    <strong>
+                                        Synonyms:
+                                    </strong>
+                                    {definition.synonyms.map(function (synonym, index) {
+                                        return (
+                                            <span key={index}>
+                                                {synonym + " "}
+                                            </span>
+                                        )
+                                    })}
+                                </>}
+                        </p>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
